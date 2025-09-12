a06cddce559b3108686a731c66b05e59e1565bc4
Kim Guerrette <kguerrette@gmail.com>
2025-09-12 13:46:40 -0700

    Adding previously unstagged changes I meant to include.

--- rw_visual.py
+++ rw_visual.py
@@ -7,16 +7,28 @@ keep_running = 'y'
 while keep_running == 'y':
 
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize=(10,6), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     # Set the aspect of the axes. 1 = 'equal'.
     # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.set_aspect.html 
     ax.set_aspect(1)
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+
     plt.show()
 
-    keep_running = input("Make another walk? (y/n): ")
+    # keep_running = input("Make another walk? (y/n): ")
+    keep_running = 'n'
